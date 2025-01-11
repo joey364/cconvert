2646bb96f5b5c74645bab13e4d147b5daed87877
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-11 23:45:01 +0000

    add transaction service

---
+++ cconvert-backend/src/transactions/transactions.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class TransactionsService {
+  constructor(private readonly prisma: PrismaService) {
+
+  }
+  async getUserTransactionsByUserId(userId: string) {
+    // TODO: paginate this
+    return await this.prisma.transaction.findMany({ where: { userId } })
+  }
+}
