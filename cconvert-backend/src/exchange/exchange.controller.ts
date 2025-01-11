d1e03852e2ba43f83ff607e95641f90357362a6b
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-11 23:44:01 +0000

    add exchange module, service and controller

---
+++ cconvert-backend/src/exchange/exchange.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ExchangeService } from './exchange.service';
+import { ConvertRequest } from 'src/dto/exchange/convert.request.dto';
+import { User } from 'src/users/user.decorator';
+
+@Controller()
+export class ExchangeController {
+  constructor(private readonly exchangeService: ExchangeService) { }
+
+  @Get('exchange-rates')
+  async getExchangeRates() {
+    return this.exchangeService.getExchangeRates()
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('convert')
+  async convertCurrency(@Body() request: ConvertRequest,  @User() user) {
+    return this.exchangeService.convert(request, user.userId)
+  }
+}
---
+++ cconvert-backend/src/exchange/exchange.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import { RatesResponse } from 'src/dto/exchange/rates.response.dto';
+import { ConvertRequest } from 'src/dto/exchange/convert.request.dto';
+import { PrismaService } from 'src/prisma.service';
+import { ConvertResponse } from 'src/dto/exchange/convert.response.dto';
+
+@Injectable()
+export class ExchangeService {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly prisma: PrismaService
+  ) { }
+
+  #API_BASE_URL = this.config.get("EXCHANGE_API_BASE_URL")
+  #API_APP_ID = this.config.get("APP_ID")
+
+  async getExchangeRates() {
+    const response = await axios.get(`${this.#API_BASE_URL}/latest.json?app_id=${this.#API_APP_ID}`)
+    const ratesResponse = new RatesResponse
+
+    ratesResponse.baseCurrency = response.data.base
+    ratesResponse.rates = response.data.rates
+
+    return ratesResponse
+  }
+
+  async convert(request: ConvertRequest, userId: string) {
+    const { rates } = await this.getExchangeRates()
+
+    const rate = rates[request.toCurrency] / rates[request.fromCurrency]
+    const convertedValue = rate * request.amount
+
+    await this.prisma.transaction.create({
+      data: { ...request, userId, convertedAmount: convertedValue }
+    })
+
+    const response = new ConvertResponse()
+    response.amount = request.amount
+    response.toCurrency = request.toCurrency
+    response.fromCurrency = request.fromCurrency 
+    response.convertedValue = convertedValue
+
+    return response
+  }
+}
