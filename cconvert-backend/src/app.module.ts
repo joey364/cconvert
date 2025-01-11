19e0078040a8f8656aa926ac80263ad4fd6a4f86
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-11 23:45:23 +0000

    update app.module.ts

--- cconvert-backend/src/app.module.ts
+++ cconvert-backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma.service';
+import { AuthController } from './auth/auth.controller';
+import { UsersService } from './users/users.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthService } from './auth/auth.service';
+import { ExchangeModule } from './exchange/exchange.module';
+import { TransactionsService } from './transactions/transactions.service';
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot({ isGlobal: true }), ExchangeModule,],
+  controllers: [AuthController],
+  providers: [UsersService, PrismaService, AuthService, TransactionsService],
 })
-export class AppModule {}
+export class AppModule { }
