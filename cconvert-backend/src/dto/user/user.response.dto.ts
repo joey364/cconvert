1977190c363a45b9ee57c49791cc87eb7e106936
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-11 23:33:18 +0000

    add user dtos

---
+++ cconvert-backend/src/dto/user/user.response.dto.ts
@@ -0,0 +1,16 @@
+import { User } from "@prisma/client"
+import { TransactionResponse } from "../transaction/transaction.response.dto"
+
+export class UserResponse {
+  id: string
+  email: string
+  transactions: TransactionResponse[]
+
+  static fromUser(user: User): UserResponse {
+    if (user == null) return null
+    const response = new UserResponse()
+    response.id = user.id
+    response.email = user.email
+    return response;
+  }
+}
