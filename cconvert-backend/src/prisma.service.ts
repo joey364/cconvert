01988631160604c87e6de6af73fc83e5bd03288e
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-11 23:27:26 +0000

    add prisma service

---
+++ cconvert-backend/src/prisma.service.ts
@@ -0,0 +1,35 @@
+import { INestApplication, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL')
+        }
+      }
+    });
+  }
+  private readonly logger = new Logger(PrismaService.name)
+
+  async onModuleInit() {
+    try {
+      this.logger.log("Connecting to the database")
+      await this.$connect();
+    } catch {
+      this.logger.warn("Failed to connect to the db")
+    }
+  }
+
+  async onModuleDestroy() {
+    try {
+      await this.$disconnect();
+    } catch {
+      this.logger.warn("Failed to disconnect from the db properly")
+    }
+
+  }
+}
