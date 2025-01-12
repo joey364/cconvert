10a7d1e6c0024d1cc88ff512120c4bcddbff91a4
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-12 05:57:52 +0000

    add user controller

--- cconvert-backend/src/users/users.controller.ts
+++ cconvert-backend/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { TransactionsService } from 'src/transactions/transactions.service';
+import { User } from './user.decorator';
 
-@Controller('users')
-export class UsersController {}
+@Controller('user')
+export class UsersController {
+  constructor(
+    private readonly transactionService: TransactionsService
+  ) { }
+
+  @UseGuards(AuthGuard)
+  @Get('transactions')
+  async getUserTransactions(@User() user) {
+  return  await this.transactionService.getUserTransactionsByUserId(user.userId)
+  }
+}
