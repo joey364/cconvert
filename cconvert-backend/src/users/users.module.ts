ac47c53ba1a719a83a29cff527ada4604f6d6a47
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-12 05:27:48 +0000

    add user controller

--- cconvert-backend/src/users/users.module.ts
+++ cconvert-backend/src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma.service';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [],
   providers: [UsersService, PrismaService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
