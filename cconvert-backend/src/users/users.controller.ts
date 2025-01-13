59f9e2a8679c5425f4ec8abe9b2acf47ec3b2798
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-13 00:36:59 +0000

    update user controller with updated transactions service method signature

--- cconvert-backend/src/users/users.controller.ts
+++ cconvert-backend/src/users/users.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { TransactionsService } from 'src/transactions/transactions.service';
 import { User } from './user.decorator';
 
 @Controller('user')
 export class UsersController {
-  constructor(
-    private readonly transactionService: TransactionsService
-  ) { }
+  constructor(private readonly transactionService: TransactionsService) {}
 
   @UseGuards(AuthGuard)
   @Get('transactions')
-  async getUserTransactions(@User() user) {
-  return  await this.transactionService.getUserTransactionsByUserId(user.userId)
+  async getUserTransactions(
+    @User() user,
+    @Query() page: number = 1,
+    @Query() limit: number = 10,
+  ) {
+    return await this.transactionService.getUserTransactionsByUserId(
+      user.userId,
+      page,
+      limit,
+    );
   }
 }
