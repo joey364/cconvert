554eee9f042918e3328c12d892a8ca6871f56202
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-15 21:44:52 +0000

    ad utils

---
+++ cconvert-frontend/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+import { randomBytes } from "crypto"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function generateNonce() {
+  return `${Date.now()}-${randomBytes(8).toString('hex')}`
+}
