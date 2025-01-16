4dc437d6ccc34d33d4f4e554984cffa0d186699d
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-16 15:24:23 +0000

    add pagination to transaction responses

--- cconvert-backend/src/transactions/transactions.service.spec.ts
+++ cconvert-backend/src/transactions/transactions.service.spec.ts
@@ -2,10 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsService } from './transactions.service';
 import { PrismaService } from 'src/prisma.service';
 import { ConfigService } from '@nestjs/config';
+import PaginationDto from 'src/dto/PaginationDto';
+import { TransactionResponse } from './dto/transaction.response.dto';
+import { Decimal } from '@prisma/client/runtime/library';
+import { Transaction } from '@prisma/client';
 
 const mockPrismaService = {
   transaction: {
     findMany: jest.fn(),
+    count: jest.fn(),
   },
 };
 
@@ -31,17 +36,68 @@ describe('TransactionsService', () => {
   describe('getUserTransactionsByUserId', () => {
     it('should return user transactions', async () => {
       const userId = 'user-id';
-      const mockTransactions = [
-        { id: 1, amount: 100, fromCurrency: 'USD', toCurrency: 'EUR' },
-        { id: 2, amount: 200, fromCurrency: 'USD', toCurrency: 'GBP' },
+      const now = new Date();
+      const mockTransactions: Transaction[] = [
+        {
+          id: 1,
+          amount: new Decimal(100),
+          fromCurrency: 'USD',
+          toCurrency: 'EUR',
+          convertedAmount: new Decimal(110),
+          userId,
+          timestamp: now,
+        },
+        {
+          id: 2,
+          amount: new Decimal(200),
+          fromCurrency: 'USD',
+          toCurrency: 'GBP',
+          convertedAmount: new Decimal(115),
+          userId,
+          timestamp: now,
+        },
       ];
+
+      const mockTransactionsResponses: TransactionResponse[] = [
+        {
+          id: 1,
+          amount: 100,
+          fromCurrency: 'USD',
+          toCurrency: 'EUR',
+          convertedAmount: 110,
+          userId,
+          timestamp: now.getMilliseconds(),
+        },
+        {
+          id: 2,
+          amount: 200,
+          fromCurrency: 'USD',
+          toCurrency: 'GBP',
+          convertedAmount: 115,
+          userId,
+          timestamp: now.getMilliseconds(),
+        },
+      ];
+
+      const paginationResponse = new PaginationDto<TransactionResponse>(
+        1,
+        10,
+        2,
+        mockTransactionsResponses,
+      );
+
       mockPrismaService.transaction.findMany.mockResolvedValue(
         mockTransactions,
       );
 
+      mockPrismaService.transaction.count.mockResolvedValue(
+        mockTransactions.length,
+      );
+
       const result = await service.getUserTransactionsByUserId(userId, 1, 10);
 
-      expect(result).toEqual(mockTransactions);
+      expect(result.totalItems).toBe(paginationResponse.totalItems);
+      expect(result.data.length).toBe(paginationResponse.data.length);
       expect(mockPrismaService.transaction.findMany).toHaveBeenCalledWith({
         where: { userId },
         skip: 0,
@@ -51,19 +107,69 @@ describe('TransactionsService', () => {
 
     it('should handle pagination correctly', async () => {
       const userId = 'user-id';
-      const mockTransactions = [
-        { id: 1, amount: 100, fromCurrency: 'USD', toCurrency: 'EUR' },
-        { id: 2, amount: 200, fromCurrency: 'USD', toCurrency: 'GBP' },
+      const now = new Date();
+
+      const mockTransactions: Transaction[] = [
+        {
+          id: 1,
+          amount: new Decimal(100),
+          fromCurrency: 'USD',
+          toCurrency: 'EUR',
+          convertedAmount: new Decimal(110),
+          userId,
+          timestamp: now,
+        },
+        {
+          id: 2,
+          amount: new Decimal(200),
+          fromCurrency: 'USD',
+          toCurrency: 'GBP',
+          convertedAmount: new Decimal(115),
+          userId,
+          timestamp: now,
+        },
       ];
+
+      const mockTransactionsResponses: TransactionResponse[] = [
+        {
+          id: 1,
+          amount: 100,
+          fromCurrency: 'USD',
+          toCurrency: 'EUR',
+          convertedAmount: 110,
+          userId,
+          timestamp: now.getMilliseconds(),
+        },
+        {
+          id: 2,
+          amount: 200,
+          fromCurrency: 'USD',
+          toCurrency: 'GBP',
+          convertedAmount: 115,
+          userId,
+          timestamp: now.getMilliseconds(),
+        },
+      ];
+
+      const paginatedResponse = new PaginationDto<TransactionResponse>(
+        2,
+        2,
+        2,
+        mockTransactionsResponses,
+      );
 
       // Simulating pagination for page 2 (skip 2, take 2)
       mockPrismaService.transaction.findMany.mockResolvedValue(
         mockTransactions,
       );
 
+      mockPrismaService.transaction.count.mockResolvedValue(
+        mockTransactions.length,
+      );
       const result = await service.getUserTransactionsByUserId(userId, 2, 2);
 
-      expect(result).toEqual(mockTransactions);
+      expect(result.totalItems).toEqual(paginatedResponse.totalItems);
+      expect(result.data.length).toEqual(paginatedResponse.data.length);
       expect(mockPrismaService.transaction.findMany).toHaveBeenCalledWith({
         where: { userId },
         skip: 2,
@@ -74,10 +180,17 @@ describe('TransactionsService', () => {
     it('should return an empty array if no transactions are found', async () => {
       const userId = 'user-id';
       mockPrismaService.transaction.findMany.mockResolvedValue([]);
+      mockPrismaService.transaction.count.mockResolvedValue(0);
+      const paginatedResponse = new PaginationDto<TransactionResponse>(
+        1,
+        10,
+        0,
+        [],
+      );
 
       const result = await service.getUserTransactionsByUserId(userId, 1, 10);
 
-      expect(result).toEqual([]);
+      expect(result).toEqual(paginatedResponse);
       expect(mockPrismaService.transaction.findMany).toHaveBeenCalledWith({
         where: { userId },
         skip: 0,
--- cconvert-backend/src/users/users.controller.ts
+++ cconvert-backend/src/users/users.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { TransactionsService } from 'src/transactions/transactions.service';
 import { User } from './user.decorator';
+import IUser from './IUser';
+import { QueryFilter } from 'src/dto/QueryFilter';
 
 @Controller('user')
 export class UsersController {
@@ -9,15 +11,11 @@ export class UsersController {
 
   @UseGuards(AuthGuard)
   @Get('transactions')
-  async getUserTransactions(
-    @User() user,
-    @Query() page: number = 1,
-    @Query() limit: number = 10,
-  ) {
+  async getUserTransactions(@User() user: IUser, @Query() filter: QueryFilter) {
     return await this.transactionService.getUserTransactionsByUserId(
       user.userId,
-      page,
-      limit,
+      filter.page,
+      filter.limit,
     );
   }
 }
