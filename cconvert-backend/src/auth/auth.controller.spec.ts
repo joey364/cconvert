882a4ced6dbc6e5680fd226005e834d518b91347
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-16 15:22:13 +0000

    update auth service to use LoginResponse dto

--- cconvert-backend/src/auth/auth.controller.spec.ts
+++ cconvert-backend/src/auth/auth.controller.spec.ts
@@ -35,7 +35,7 @@ describe('AuthController', () => {
   });
 
   describe('login', () => {
-    it('should return a token if login is successful', async () => {
+    it('should return a login response if login is successful', async () => {
       // Arrange
       const loginRequest: LoginRequest = {
         email: 'test@example.com',
@@ -43,13 +43,16 @@ describe('AuthController', () => {
       };
       const mockToken = 'mockToken123';
 
-      authService.login = jest.fn().mockResolvedValue(mockToken);
+      const loginResponse = new LoginResponse();
+      loginResponse.token = mockToken;
+
+      authService.login = jest.fn().mockResolvedValue(loginResponse);
 
       // Act
       const result = await controller.login(loginRequest);
 
       // Assert
-      expect(result).toBe(mockToken);
+      expect(result).toStrictEqual(loginResponse);
       expect(authService.login).toHaveBeenCalledWith(loginRequest);
     });
 
