c2a90df4186552e46b23ff8c7defd3f8b32a975a
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-16 15:26:49 +0000

    update cors config

--- cconvert-backend/src/main.ts
+++ cconvert-backend/src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { CatchEverythingFilter } from './filters/catchEverytingFilter';
+import { deploymentConstants } from './config/deployment.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,9 +13,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
     methods: ['GET', 'POST'],
-    origin: 'localhost',
+    origin: [
+      deploymentConstants['cconvert-frontend'],
+      deploymentConstants['cconvert-local'],
+    ],
   });
 
-  await app.listen(process.env.PORT ?? 8088);
+  await app.listen(deploymentConstants.port);
 }
 bootstrap();
