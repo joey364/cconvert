8f30b3dfab4d513c3269f0559eefa1110cd84ad6
joey364 <71462893+joey364@users.noreply.github.com>
2025-01-16 15:25:18 +0000

    add catchEverythingFilter

--- cconvert-backend/src/main.ts
+++ cconvert-backend/src/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CatchEverythingFilter } from './filters/catchEverytingFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CatchEverythingFilter(httpAdapterHost));
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
     methods: ['GET', 'POST'],
